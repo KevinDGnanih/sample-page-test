fbc24e1e4c4920c3c2fe6f7a5e0d269b2dfecaf8
Your Name <you@example.com>
2023-10-17 10:13:51 +0100

    Working on the posts layout card

--- components/cards/PostCard.tsx
+++ components/cards/PostCard.tsx
@@ -3,15 +3,35 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 const PostCard = () => {
-  return (
-    <article className='d-flex container'>
-      <div className='col-lg-3 col-md-6'>
-        <div className='mb-4 roudned-4 shadow-sm bg-white'>
-            
+return (
+<article className='d-flex'>
+    <header className='mb-4 rounded-4 shadow-sm bg-white'>
+        <div className='d-flex col-12 p-4'>
+            <Image src="/head-01.jpg" width={45} height={45} alt='Profile picture' />
+            <div className='ms-2 row lh-1 align-items-center'>
+                <p className='d-flex fw-semibold'>Marina Valentine
+                    <i className="ms-1 me-2 text-primary bi bi-check-circle-fill"></i>
+                    <span className='badge text-bg-primary rounded-pill'>Pro Member</span>
+                </p>
+                <p className='text-secondary'>replied to the topic
+                    <span className='fw-semibold'> Welcome to the Cosplayers Group Forum!</span>
+                </p> 
+                <p className='text-secondary'>in the forum
+                    <span className='fw-semibold'> Cosplayers Group Forum</span>
+                </p>
+                <span className='text-secondary'>1 year ago</span>
+            </div>
         </div>
-      </div>
-    </article>
-  )
+        <div className='d-flex col-12 p-4'>
+            <p>Hello Everyone!</p>
+        </div>
+
+        <hr className='m-3 border border-1 opacity-50' />
+
+
+    </header>
+</article>
+)
 }
 
 export default PostCard
